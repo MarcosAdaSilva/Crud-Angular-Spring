b41eb7d44fff71bfaf9ab78448c069bd45446f6b
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-03 00:17:54 -0300

    criando chamada HTTP Get Angular e RXJS

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [{ _id: '1', name: 'Angular', category: 'front-end' }];
+  list() {
+    return this.httpClient.get<Course[]>(this.API);
   }
 }
