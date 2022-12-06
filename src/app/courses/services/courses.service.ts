2ffb9787c1c18174a83f6dede08e0276c1032667
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-06 00:17:57 -0300

    finalizando chamadas HTTP

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { observable } from 'rxjs';
+import { delay, first, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
