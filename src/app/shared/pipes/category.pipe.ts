e8a0f436d249ae4173ea5364cc904ed6c001468c
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-08 23:53:28 -0300

    Lista de cursos Pipe / icons

---
+++ src/app/shared/pipes/category.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'category',
+})
+export class CategoryPipe implements PipeTransform {
+  transform(value: string, ...args: unknown[]): string {
+    return null;
+  }
+}
