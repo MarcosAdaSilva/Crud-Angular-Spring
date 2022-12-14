9e4333fed96249a5daf8a2205d1480be854e5eab
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-14 00:01:19 -0300

    spring

--- crud-spring/crud-spring/src/main/java/controller/HelloController.java
+++ crud-spring/crud-spring/src/main/java/controller/HelloController.java
@@ -2,22 +2,15 @@ package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
 
-
   @GetMapping
-
-  public String hello() {
-
-
+  public String hello(){
     return "Hello!";
-
-
   }
 
 }
