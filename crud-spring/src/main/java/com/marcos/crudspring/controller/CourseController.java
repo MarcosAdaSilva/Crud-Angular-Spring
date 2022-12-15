7f7f12023af4bd4bf5501005ee925714ae86320f
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-15 00:11:26 -0300

    Criando Model Course

--- crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
@@ -1,9 +1,8 @@
 package com.marcos.crudspring.controller;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
---
+++ crud-spring/src/main/java/com/marcos/crudspring/model/Course.java
@@ -0,0 +1,11 @@
+package com.marcos.crudspring.model;
+
+public class Course {
+
+  private Long id;
+
+  private String name;
+
+  private String category;
+
+}
