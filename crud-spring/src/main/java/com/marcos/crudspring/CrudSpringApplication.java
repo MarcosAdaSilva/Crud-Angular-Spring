a9ec365b1f6259ad90deb14d348209ec424f5fde
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-15 01:29:45 -0300

    melhorando o código

--- crud-spring/src/main/java/com/marcos/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/marcos/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.marcos.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.marcos.crudspring.model.Course;
+import com.marcos.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -9,5 +14,21 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
+
+  @Bean
+  CommandLineRunner initDatabase(CourseRepository courseRepository) {
+    return args -> {
+      courseRepository.deleteAll();
+
+      Course c = new Course();
+      c.setName("Angular com Spring");
+      c.setCategory("Front-end");
+
+
+      courseRepository.save(c);
+
+    };
+
+  }
 
 }
