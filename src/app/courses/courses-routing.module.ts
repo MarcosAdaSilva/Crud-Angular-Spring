b7582119ffb8032554b8fb60b32f969527e12594
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-23 00:23:32 -0300

    Adicionando botão de incluir

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,7 +1,7 @@
-import { CourseFormComponent } from './course-form/course-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
