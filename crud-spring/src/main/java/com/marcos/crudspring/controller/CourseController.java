a28718dce66b51ff09b6c053ce726f28d53d3f01
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-29 00:13:00 -0300

    Trabalhando no Backend

--- crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,8 @@ public class CourseController {
   }
 
   //@RequestMapping(method = RequestMethod.POST)
-  public void create() {
+  @PostMapping
+  public void create(@RequestBody Course course) {
 
   }
 
