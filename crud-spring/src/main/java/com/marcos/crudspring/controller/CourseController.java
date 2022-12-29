65bae0ca986fcece0f5c3e8b8b4cfa0386bc032a
MarcosAdaSilva <marcaofly@gmail.com>
2022-12-29 00:30:33 -0300

    Update CourseController.java

--- crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/marcos/crudspring/controller/CourseController.java
@@ -5,6 +5,8 @@ import com.marcos.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,8 +32,12 @@ public class CourseController {
 
   //@RequestMapping(method = RequestMethod.POST)
   @PostMapping
-  public void create(@RequestBody Course course) {
+  public ResponseEntity<Course> create(@RequestBody Course course) {
 
+    // System.out.println(course.getName());
+
+   // return courseRepository.save(course);
+   return ResponseEntity.status(HttpStatus.CREATED.  body(courseRepository.save(course));
   }
 
 }
